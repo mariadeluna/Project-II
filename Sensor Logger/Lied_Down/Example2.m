ea0f87ff8cbb319417ab51a5f04d168c44cd769e
María de Luna <127328110+mariadeluna@users.noreply.github.com>
2024-04-03 14:38:28 +0200

    Add files via upload

--- Sensor Logger/Lied_Down/Example2.m
+++ Sensor Logger/Lied_Down/Example2.m
@@ -1,47 +1,53 @@
+
 name = 'Accelerometer.csv';
 
 
-
 opts = detectImportOptions(name);
-opts.SelectedVariableNames = [2 3 4 5];  % Seleccionamos solo las columnas de interés, aquí la segunda y tercera
-datos = readtable(name, opts);
-datosArray = table2array(datos);
-
-% Extraer el vector de tiempo y los datos del eje z
-tiempo = datos{1465:3800, 1}; % Asumiendo que la primera columna seleccionada es el tiempo
-ejes = datosArray(1465:3800, 2:4);   % Asumiendo que la segunda columna seleccionada son los datos del eje z
+opts.SelectedVariableNames = [2 3 4 5];  
+data = readtable(name, opts);
+dataArray = table2array(data);
 
 
- Fs=1000;
-% f_c=40;
-% Wn = f_c/(Fs/2); % Normalize
-% N=6;
-% [b, a] = butter(N, Wn, 'low'); % Filter coefficients
-% 
-% 
-% ejes_filt = filtfilt(b, a, ejes);
-% 
-% 
-% 
-% % Graficar la señal del eje z
-% figure;
-% plot(tiempo, ejes_filt);
-% xlabel('Tiempo (s)');
-% ylabel('Amplitud del eje Z');
-% title('Datos del Acelerómetro - Eje Z');
-
-f_pb = [0.8,2];  % Frecuencia de corte del filtro pasa banda
-[b, a] = butter(2, f_pb/(Fs/2), 'bandpass');  % Diseño del filtro pasa banda
-ejes_f = filtfilt(b, a, ejes);  % Aplicación del filtro
+t = data{1500:3800, 1}; 
+subs =diff(t);
+sample_interval=mean(subs);
+ Fs=1/sample_interval;
 
 
-% Graficar la señal suavizada
+
+scg_x=data{1500:3800, 4}; 
+scg_y=data{1500:3800, 3}; 
+scg_z=data{1500:3800, 2}; 
+
+% Design a bandpass filter using the designfilt function
+bpFilt = designfilt('bandpassiir', 'FilterOrder', 6, ...
+         'HalfPowerFrequency1', 0.3, 'HalfPowerFrequency2', 5, ...
+         'SampleRate', Fs); 
+
+% Apply the filter
+scg_x_filtered = filtfilt(bpFilt, scg_x);
+scg_y_filtered = filtfilt(bpFilt, scg_y);
+scg_z_filtered = filtfilt(bpFilt, scg_z);
+
+
 figure;
-plot(tiempo, ejes_f, 'LineWidth', 1);
-xlabel('Tiempo (s)');
-ylabel('Amplitud');
-title('Datos del Acelerómetro Suavizados para Análisis de Pulso Cardiaco');
-grid on;
+subplot(3,1,1); % First graphic scg
+plot(t, scg_x_filtered);
+title('Acceleration in X');
+xlabel('Time (s)');
+ylabel(' (m/s^2)');
 
 
-%--------------------------------------------
+subplot(3,1,2); % Second graphic 
+plot(t, scg_y_filtered);
+title('Acceleration in Y');
+xlabel('Time (s)');
+ylabel('(m/s^2)');
+
+
+
+subplot(3,1,3); % Third graphic
+plot(t, scg_z_filtered);
+title('Acceleration in Z');
+xlabel('Time (s)');
+ylabel('(m/s^2)');
